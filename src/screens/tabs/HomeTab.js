4c37f0d3cc76b66a6fd0b709c1398f19f91ecf20
Kim McCaskill <kimmccaskill92@gmail.com>
2020-04-06 18:41:48 -0600

    Clean up extra code

--- src/screens/tabs/HomeTab.js
+++ src/screens/tabs/HomeTab.js
@@ -1,48 +1,20 @@
-import React,{ useState } from 'react';
-import Form from '../../components/Form/Form'
-import ResultPage from '../../components/ResultPage/ResultPage'
-import { StyleSheet, Text, View, Button, Image } from 'react-native';
+import React from 'react'
+import FormScreen from '../FormScreen'
+import ResultScreen from '../ResultScreen'
+import StartScreen from '../StartScreen'
+
+import { createStackNavigator } from '@react-navigation/stack'
+
+const Stack = createStackNavigator();
 
 const HomeTab = () => {
-  const [showForm, setShowForm] = useState(false)
-  const [showResult, setShowResult] = useState(false)
-  
   return (
-    <View style={styles.container}>
-      <Image style={styles.eightBall} source={require('../../../assets/ate-ball-home.png')}/>
-      <View style={styles.startBtn}>
-        <Button onPress={() => setShowForm(true)} title="Get Started"/>
-        <Form visible={showForm} setShowForm={setShowForm} setShowResult={setShowResult}/>
-        <ResultPage visible={showResult}/>
-      </View>
-    </View>
+    <Stack.Navigator initialRouteName="Home">
+      <Stack.Screen name="Hangry Ateball" component={StartScreen} />
+      <Stack.Screen name="Form" component={FormScreen} />
+      <Stack.Screen name="Result" component={ResultScreen} />
+    </Stack.Navigator>
   )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    paddingTop: 50,
-    alignItems: 'center',
-    width: '100%',
-  },
-  header: {
-    width: '100%',
-    borderBottomWidth: 2,
-    borderBottomColor: 'black'
-  },
-  title: {
-    fontSize: 20,
-    paddingBottom: 10,
-    alignSelf: 'center'
-  },
-  startBtn: {
-    width: '80%',
-  },  
-  eightBall: {
-    width: 350,
-    resizeMode: 'contain'
-  }
-});
-
 
 export default HomeTab;

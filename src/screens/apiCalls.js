a07927fde4277ef11882411d52b67a414daf7f51
Caleb Haizlett <caleb.haizlett@gmail.com>
2020-04-11 14:26:57 -0600

    Add async/await to fetches

--- src/screens/apiCalls.js
+++ src/screens/apiCalls.js
@@ -1,15 +1,17 @@
-export const getRestaurantData = (userLocation, restaurantType, price) => {
-  if(restaurantType && cost) {
-    return fetch(`https://hangry-ateball-staging.herokuapp.com/api/v1/recommendations?latitude=${userLocation.latitude}&longitude=${userLocation.longitude}&categories=${restaurantType}&price=${price}`) 
+export const getRestaurantData = async (userLocation, restaurantType, price) => {
+  console.log(userLocation, restaurantType, price, 'params');
+  if(restaurantType && price) {
+    return await fetch(`https://hangry-ateball-staging.herokuapp.com/api/v1/recommendations?latitude=${userLocation.latitude}&longitude=${userLocation.longitude}&categories=${restaurantType}&price=${price}`) 
       .then(response => response.json())
+      .catch(error => console.log('error fetching'))
   } else if (restaurantType) {
-    return fetch(`https://hangry-ateball-staging.herokuapp.com/api/v1/recommendations?latitude=${userLocation.latitude}&longitude=${userLocation.longitude}&categories=${restaurantType}`) 
+    return await fetch(`https://hangry-ateball-staging.herokuapp.com/api/v1/recommendations?latitude=${userLocation.latitude}&longitude=${userLocation.longitude}&categories=${restaurantType}`) 
       .then(response => response.json())
   } else if (price) {
-    return fetch(`https://hangry-ateball-staging.herokuapp.com/api/v1/recommendations?latitude=${userLocation.latitude}&longitude=${userLocation.longitude}&price=${price}`) 
+    return await fetch(`https://hangry-ateball-staging.herokuapp.com/api/v1/recommendations?latitude=${userLocation.latitude}&longitude=${userLocation.longitude}&price=${price}`) 
       .then(response => response.json())
   } else {
-    return fetch(`https://hangry-ateball-staging.herokuapp.com/api/v1/recommendations?latitude=${userLocation.latitude}&longitude=${userLocation.longitude}`) 
+    return await fetch(`https://hangry-ateball-staging.herokuapp.com/api/v1/recommendations?latitude=${userLocation.latitude}&longitude=${userLocation.longitude}`) 
       .then(response => response.json())
   }  
 }

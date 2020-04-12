b2e4e85a47a0f03e99ce9640d103dfb6442e2ca3
Kim McCaskill <kimmccaskill92@gmail.com>
2020-04-12 14:00:27 -0600

    Add tests for after clicking Shake It

--- cypress/integration/App_spec.js
+++ cypress/integration/App_spec.js
@@ -2,24 +2,47 @@ describe('My First Test', () => {
   beforeEach(() => {
     cy.visit("http://localhost:19006/")
   })
-  it('Does not do much!', () => {
-    expect(true).to.equal(true)
-  })
   it('should have Get Shakin button on page load', () => {
       cy.get('div')
       cy.contains('Get Shakin')
   })
 
-  it.only('should load form labels properly', () => {
+  describe('After clicking Get Shakin', () => {
+    beforeEach(() => {
       cy.get('div').contains('Get Shakin').click()
+    })
+    it('should load form labels properly', () => {
       cy.contains('Type')
       cy.contains('Travel')
       cy.contains('Cost')
+    })
+    it('should load form inputs properly', () => {
+      cy.get('select')
+      cy.get('option')
+    })
   })
-  it.only('should load form inputs properly', () => {
-    cy.get('div').contains('Get Shakin').click()
-    cy.get('select')
-    cy.get('option')
+
+  describe('After clicking Shake It', () => {
+    beforeEach(() => {
+      cy.get('div').contains('Get Shakin').click()
+      cy.get('select').get('[data-testid="Cost"]').select("3")
+      cy.get('div').contains('Shake It').click()
+    })
+    it('should load title properly', () => {
+      cy.get('[aria-label="title"]')
+    })
+    it('should load phone number properly', () => {
+      cy.get('[datadetectortype="phoneNumber"]')
+    })
+    it.skip('should load cost that user selected', () => {
+      cy.get('div').contains("3")
+    })
+    it('should load button properly', () => {
+      cy.get('[role="button"]')
+    })
+    it('should load images properly', () => {
+      cy.get('img')
+    })
   })
 
 })

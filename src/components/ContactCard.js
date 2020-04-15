232040c54e05b07ec9ec9990d00682d5de77e9bd
Kim McCaskill <kimmccaskill92@gmail.com>
2020-04-14 21:14:43 -0600

    Add stylesheet to contactcard

--- src/components/ContactCard.js
+++ src/components/ContactCard.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { View, Text, TouchableOpacity } from 'react-native'
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native'
 import { sendSMS } from '../apiCalls'
 
 const ContactCard = ({ name, phoneNumber, digits, message, closeModal}) => {

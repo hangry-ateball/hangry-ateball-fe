93fcf89fbef1b865e05ff653049f1e11a39cc59a
Caleb Haizlett <caleb.haizlett@gmail.com>
2020-04-14 23:41:52 -0600

    Import and add favorite functions

--- src/screens/asyncStorageHelper.js
+++ src/screens/asyncStorageHelper.js
@@ -1,27 +1,63 @@
 import { AsyncStorage } from 'react-native';
 
-export const fetchPreviousRestaurants = async () => {
+export const fetchRestaurants = async (key) => {
   try {
-    let previousRestaurants = await AsyncStorage.getItem('previous');
-    if (previousRestaurants === null) { 
+    let restaurants = await AsyncStorage.getItem(key);
+    if (restaurants === null) { 
       return []; 
     }
-    return parsePreviousRestaurants(previousRestaurants);
+    return parseRestaurants(restaurants);
   } catch (error) {
     console.log('Error fetching previous restaurants', error);
   }
 }
 
-const parsePreviousRestaurants = (previousRestaurants) => {
-  return JSON.parse(previousRestaurants).filter((restaurant, i) => {
-    return JSON.parse(previousRestaurants).map(restaurant => restaurant.name).indexOf(restaurant.name) === i
+export const mergeRestaurants = (favoriteRestaurants, restaurant) => {
+  return [...favoriteRestaurants, {name: restaurant.name, rating: restaurant.rating, price: restaurant.price}]
+}
+
+export const saveRestaurants = (previousRestaurants, key) => {
+  AsyncStorage.setItem(key, JSON.stringify(previousRestaurants));
+}
+
+const parseRestaurants = (restaurants) => {
+  return JSON.parse(restaurants).filter((restaurant, i) => {
+    return JSON.parse(restaurants).map(restaurant => restaurant.name).indexOf(restaurant.name) === i
   })
 }
 
-export const mergePreviousRestaurants = (previousRestaurants, restaurant) => {
-  return [...previousRestaurants, {name: restaurant.name, rating: restaurant.rating, price: restaurant.price}];
+export const updatePreviousRestaurants = async (restaurant) => {
+  try {
+    let previous = await fetchRestaurants('previous');
+    previous = mergeRestaurants(previous, restaurant);
+    saveRestaurants(previous, 'previous');
+  } catch (error) {
+    console.log('Error fetching Previous', error);
+  }
 }
 
-export const savePreviousRestaurants = (previousRestaurants) => {
-  AsyncStorage.setItem('previous', JSON.stringify(previousRestaurants));
+export const addFavoriteRestaurant = async (restaurant) => {
+  try {
+    let favorites = await fetchRestaurants('favorite');
+    favorites = mergeRestaurants(favorites, restaurant)
+    saveRestaurants(favorites, 'favorite');
+  } catch (error) {
+    console.log('Error fetching favorites', error);
+  }
+}
+
+export const removeFavoriteRestaurant = (favoriteRestaurants, unfavoritedRestaurant) => {
+  return favoriteRestaurants.filter(restaurant => {
+    return restaurant.name !== unfavoritedRestaurant.name
+  });
+}
+
+export const unFavoriteRestaurant = async (restaurant) => {
+  try {
+    let favorites = await fetchRestaurants('favorite')
+    favorites = removeFavoriteRestaurant(favorites, restaurant)
+    saveRestaurants(favorites, 'favorite')
+  } catch (error) {
+    console.log('Error fetching favorites', error);
+  }
 }

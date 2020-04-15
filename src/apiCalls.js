f7256301f8de50a18bde7c1763858dfd1198db41
Kim McCaskill <kimmccaskill92@gmail.com>
2020-04-14 21:14:00 -0600

    Add use of staging API

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,3 +1,3 @@
 export const sendSMS = (to, from, body) => {
-  return fetch(`https://hangry-ateball-api.herokuapp.com/api/v1/notify?to=${to}&from=${from}&body=${body}`)
+  return fetch(`https://hangry-ateball-staging.herokuapp.com/api/v1/notify?to=${to}&from=${from}&body=${body}`)
 }
